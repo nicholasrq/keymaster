1634476c17ee56097e6dae4242c152a5e16c0e70
Nick Skriabin <767890+nicholasrq@users.noreply.github.com>
2021-11-06 05:20:52 +0300

    Add support for multi-shortcuts

--- keymaster.js
+++ keymaster.js
@@ -103,7 +103,7 @@
           if((!_mods[k] && index(handler.mods, +k) > -1) ||
             (_mods[k] && index(handler.mods, +k) == -1)) modifiersMatch = false;
         // call the handler and stop the event if neccessary
-        if((handler.mods.length == 0 && !_mods[16] && !_mods[18] && !_mods[17] && !_mods[91]) || modifiersMatch){
+        if (_downKeys.length == (handler.mods.length + 1) && checkKeys(handler.key)) {
           if(handler.method(event, handler)===false){
             if(event.preventDefault) event.preventDefault();
               else event.returnValue = false;
@@ -114,6 +114,14 @@
       }
     }
   };
+
+  function checkKeys(key) {
+    var keys = key.split('+');
+    for (var i = 0, l = keys.length; i < l; i++) {
+      if (index(_downKeys, _MODIFIERS[keys[i]] || code(keys[i])) == -1) return false;
+    }
+    return true;
+  }
 
   // unset modifier keys on keyup
   function clearModifier(event){
@@ -135,6 +143,7 @@
   function resetModifiers() {
     for(k in _mods) _mods[k] = false;
     for(k in _MODIFIERS) assignKey[k] = false;
+    _downKeys = [];
   };
 
   // parse and assign shortcut
